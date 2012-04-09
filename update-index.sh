06f331f97e36530411e8872e6283680f6f29ce54
Julien Richard-Foy <julien@richard-foy.fr>
2012-04-09 16:22:21 +0200

    sifflote

--- update-index.sh
+++ update-index.sh
@@ -8,8 +8,7 @@ cat <<-EOF
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <title>retronym.github.com</title>
-    <script type="text/javascript" src="script.js"></script>
+    <title>julienrf.github.com</title>
   </head>
   <body>
   <h1>Repository Contents</h1>
