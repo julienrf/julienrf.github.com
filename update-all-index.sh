7e0d2a0bd9e819a37f8c57c80239228bdaac5d1a
Julien Richard-Foy <julien@richard-foy.fr>
2013-02-20 17:11:53 +0100

    play-jsmessages-1.4.1

--- update-all-index.sh
+++ update-all-index.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 for i in $(find repo -type d); do
-   (cd $i; pwd; ~/backup/julienrf.github.com/update-index.sh > index.html)
+   (cd $i; pwd; ~/sites/julienrf.github.com/update-index.sh > index.html)
 done
 
 for i in $(find repo-snapshots -type d); do
-   (cd $i; pwd; ~/backup/julienrf.github.com/update-index.sh > index.html)
+   (cd $i; pwd; ~/sites/julienrf.github.com/update-index.sh > index.html)
 done
