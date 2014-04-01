961aa63820e2d657b1e670609069541ac4dd668f
Julien Richard-Foy <julien@richard-foy.fr>
2014-04-01 18:32:27 +0200

    jsmessages-1.6.0 documentation

---
+++ play-jsmessages/1.6.0/api/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"jsmessages" : [{"class" : "jsmessages\/JsMessages.html", "name" : "jsmessages.JsMessages"}], "jsmessages.api" : [{"object" : "jsmessages\/api\/JsMessages$.html", "class" : "jsmessages\/api\/JsMessages.html", "name" : "jsmessages.api.JsMessages"}]};
